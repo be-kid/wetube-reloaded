5e5d10375fc64f8070a173856a20758031cacaf3
daramzy <daramzycoder@gmail.com>
2021-11-15 23:36:56 +0900

    login, session

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -13,6 +16,16 @@ app.use(logger);
 app.use(express.urlencoded({extended: true}));
 //form을 이해하기 위한 method
 
+app.use(
+    session({
+        secret: "hello",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube"}),
+    })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users",userRouter);
