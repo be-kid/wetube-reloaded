e181960a5721ae188fc9a9f66a43c617e63c9f40
daramzy <daramzycoder@gmail.com>
2021-11-16 22:46:52 +0900

    github login

--- src/db.js
+++ src/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 //mongoose가 오래된 것들을 처리하는 내용
-
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     //useFindAndModify: false,
     //useCreateIndex: true,
 });
-
 
 const db = mongoose.connection;
 
