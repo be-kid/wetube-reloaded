3d64cfc0f2c30855ca3c28bc57c08f2018303338
daramzy <daramzycoder@gmail.com>
2021-11-19 16:07:37 +0900

    비디오 업로드, 유저 프로필, 유저-비디오 연결

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
-import e from "express";
 
 export const getjoin = (req, res) => res.render("join", {pageTitle: "Join"});
 export const postjoin = async (req, res) => {
@@ -223,4 +223,14 @@ export const postChangePassword = async (req, res) => {
     return res.redirect("/users/logout");
 };
 
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+    const {id} = req.params;
+    const user = await User.findById(id).populate("videos");
+    if (!user){
+        return res.status(404).render("404", {pageTitle: "User not found."});
+    }
+    return res.render("users/profile", {
+        pageTitle: user.name,
+        user,
+    });
+};
