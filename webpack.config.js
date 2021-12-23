0d94c6e2421062acf553b922fbd4e7a6f1c83e54
daramzy <daramzycoder@gmail.com>
2021-12-23 21:53:01 +0900

    sad

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
         recorder: BASE_JS + "recorder.js",
         commentSection: BASE_JS + "commentSection.js",
     },
-    watch: true,
     plugins: [
         new MiniCssExtractPlugin({
             filename: "css/styles.css",
