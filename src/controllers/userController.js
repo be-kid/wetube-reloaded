6d67c34d5df293d063d269b7008e507ee9b19ec0
daramzy <daramzycoder@gmail.com>
2021-12-24 17:16:21 +0900

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -174,11 +174,11 @@ export const postEdit = async (req, res) => {
             }); 
         }
     }
-
+    console.log(file);
     const updatedUser = await User.findByIdAndUpdate(
         _id, 
         {
-            avatarUrl: file ? file.path: avatarUrl,
+            avatarUrl: file ? file.location: avatarUrl,
             name,
             email,
             username,
